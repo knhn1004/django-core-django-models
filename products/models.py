b05a12c3dd4a87fa585efb326f148a2ffa6910cb
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-22 19:22:17 +0800

    custom product manager

--- products/models.py
+++ products/models.py
@@ -1,7 +1,28 @@
-from enum import unique
 from django.db import models
 from .validators import validate_blocked_words
 from core.db.models import BasePublishModel
+from django.utils import timezone
+
+
+class ProductQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(state=Product.PublishStateOptions.PUBLISH,
+                           publish_timestamp__lte=now)
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def published(self):
+        # Product.objects.filter()
+        now = timezone.now()
+        # return self.get_queryset().filter(state=Product.PublishStateOptions.PUBLISH,
+        #                                  publish_timestamp__lte=now)
+        # Product.objects.published()
+        # Product.objects.filter(title__icontains='Title').published()
+        return self.get_queryset().published()
 
 
 class Product(BasePublishModel):
@@ -14,6 +35,9 @@ class Product(BasePublishModel):
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     #order = models.IntegerField()
+
+    ''' cutom manager '''
+    objects = ProductManager()
 
     class Meta:
         ordering = ['-updated', '-timestamp']
