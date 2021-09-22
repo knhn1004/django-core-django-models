c999a58ed4bc8bb23130f92b751ed4560e1feb97
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-22 18:59:36 +0800

    other meta classes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 from .validators import validate_blocked_words
 from core.db.models import BasePublishModel
@@ -5,11 +6,21 @@ from core.db.models import BasePublishModel
 
 class Product(BasePublishModel):
     title = models.CharField(max_length=120,
-                             validators=[
+                             validators=[  # this only validate on forms
                                  validate_blocked_words,
-                             ])  # this only validate on forms
+                             ],
+                             # unique=True
+                             )
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
+    #order = models.IntegerField()
+
+    class Meta:
+        ordering = ['-updated', '-timestamp']
+        # db_table = '<app_name>_<model_name>' # 'products_product
+        # unique_together = [['title', 'order']] # 2 or more fields that need to be unique at the same time
+        #verbose_name = 'product'
+        #verbose_name_plural = 'products'
 
     def save(self, *args, **kwargs):
         validate_blocked_words(self.title)
