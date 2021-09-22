e67cb62b35b2f75fc0d1aaf928c4839b69f90d49
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-22 15:49:00 +0800

    migrations

--- products/models.py
+++ products/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     """ a single product"""
-    title = models.TextField()
-    price = models.DecimalField()
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True)
+    #short_description = models.CharField(max_length=220)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
